18318a4364c56bb935bef7fac1472d9146a55605
Matteoguerra07 <matteo.guerra2233@gmail.com>
2025-11-26 22:23:03 +0100

    Sistemato nella classe MergeSort (metoto merge) classe Main (aggiunto menù

--- MergeSort/src/Main.java
+++ MergeSort/src/Main.java
@@ -1,5 +1,60 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Inserire dimensione dell'array: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        boolean running = true;
 
+        while (running) {
+
+            stampaMenu();
+            int opzione = input.nextInt();
+
+            switch (opzione) {
+                case 1:
+                    aggiungiElementi(arr);
+                    break;
+                case 2:
+                    stampaArray(arr);
+                    break;
+                case 3:
+                    MergeSort toSort = new MergeSort(arr);
+                    toSort.divide(0, arr.length - 1);
+                    break;
+                case 4:
+                    running = false;
+                    break;
+            }
+        }
+
+    }
+
+    public static void stampaMenu (){
+        System.out.println("\n▶----------------------------◀");
+        System.out.println("|   Algoritmo Merge Sort     |");
+        System.out.println("▶----------------------------◀");
+        System.out.println("\nSelezionare cosa si vuole fare: ");
+        System.out.println("\t🔸Aggiungere gli elementi all'array.");
+        System.out.println("\t🔸Visualizzare l'array.");
+        System.out.println("\t🔸Ordinare l'array.");
+        System.out.println("\t🔸Uscire dal programma.");
+    }
+
+    public static void aggiungiElementi (int[]arr){
+        Scanner input= new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextInt();
+        }
+    }
+
+    public static void stampaArray(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
--- MergeSort/src/MergeSort.java
+++ MergeSort/src/MergeSort.java
@@ -8,7 +8,7 @@ public class MergeSort {
 
     public void divide (int sx, int dx){
         if (sx < dx){
-            int mid = sx + (int)((dx-sx)/2);
+            int mid = sx + (dx-sx)/2;
             divide (sx,mid);
             divide(mid+1, dx);
             merge(sx,mid,dx);
@@ -17,24 +17,43 @@ public class MergeSort {
     }
 
     public void merge (int sx,int mid, int dx){
-        int n1 = mid-sx;
-        int n2 = dx - mid + 1;
+        int n1 = mid-sx + 1;
+        int n2 = dx - mid;
 
         int[]a_sx = new int[n1];
         int[]a_dx = new int[n2];
 
         for (int i = 0; i < n1; i++) {
-            a_sx[i] = this.arr[mid+i+1];
+            a_sx[i] = this.arr[sx + i];
         }
         for (int i = 0; i < n2; i++) {
-            a_dx[i] = this.arr[mid+i+1];
+            a_dx[i] = this.arr[mid + i + 1];
         }
         int o_i = sx;
         int dx_i = 0;
         int sx_i = 0;
 
-        while(sx_i < n1  && ){
+        while (sx_i < n1 && dx_i < n2) {
+            if (a_sx[sx_i] <= a_dx[dx_i]) {
+                arr[o_i] = a_sx[sx_i];
+                sx_i++;
+            } else {
+                arr[o_i] = a_dx[dx_i];
+                dx_i++;
+            }
+            o_i++;
+        }
 
+        while(sx_i < n1){
+            arr[o_i] = a_sx[sx_i];
+            sx_i++;
+            o_i++;
+        }
+
+        while(dx_i < n2){
+            arr[o_i] = a_dx[dx_i];
+            dx_i++;
+            o_i++;
         }
     }
 }
